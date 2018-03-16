bb646f631966fda54bc98efda88e599ef96bc40a
PC-20180202FXRO\Administrator <497446610@qq.com>
2018-03-16 16:51:20 +0800

    增加获取远程分支的方法

--- src/main/java/cn/kuangxf/doc/source/GitUtil.java
+++ src/main/java/cn/kuangxf/doc/source/GitUtil.java
@@ -7,11 +7,15 @@
 package cn.kuangxf.doc.source;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Ref;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -37,7 +41,7 @@ public class GitUtil {
 	 * @param localPath
 	 *            本地目录
 	 * @param branch
-	 *            分支
+	 *            分支 默认为master
 	 */
 	public static void clone(String uri, String localPath, String branch) throws Exception {
 		if (StringUtils.isEmpty(uri)) {
@@ -59,6 +63,17 @@ public class GitUtil {
 		logger.info("finish clone.");
 	}
 
+	/**
+	 * 克隆源代码到本地文件<br/>
+	 * 如果本地目录路径已经存在时，会先删除本地目录
+	 * 
+	 * @param uri
+	 *            git 地址
+	 * @param localPath
+	 *            本地目录
+	 * @param branch
+	 *            分支 默认为master
+	 */
 	public static void cloneDelFirst(String uri, String localPath, String branch) throws Exception {
 		if (StringUtils.isEmpty(uri)) {
 			throw new Exception("uri is empty.");
@@ -81,11 +96,11 @@ public class GitUtil {
 	 * @param localRepository
 	 *            本地仓库目录
 	 */
-	public static void pull(String localRepository)throws Exception  {
+	public static void pull(String localRepository) throws Exception {
 		if (StringUtils.isEmpty(localRepository)) {
 			throw new Exception("localRepository is empty.");
 		}
-		
+
 		File dest = new File(localRepository);
 		if (!dest.exists()) {
 			throw new Exception("repository is not exists.");
@@ -95,10 +110,34 @@ public class GitUtil {
 		logger.info("finish pull.");
 	}
 
+	/**
+	 * 获取分支列表
+	 * 
+	 * @param uri
+	 *            git url
+	 * @return branch list
+	 * @throws Exception
+	 */
+	public static List<String> branchList(String uri) throws Exception {
+		if (StringUtils.isEmpty(uri)) {
+			throw new Exception("uri is empty.");
+		}
+		logger.info("Listing remote repository: {}", uri);
+		Collection<Ref> refs = Git.lsRemoteRepository().setHeads(true).setTags(true).setRemote(uri).call();
+		List<String> result = new ArrayList<>();
+		int index = "refs/heads/".length();
+		for (Ref ref : refs) {
+			result.add(ref.getName().substring(index));
+		}
+		return result;
+	}
+
 	public static void main(String[] args) throws Exception {
 
-		//GitUtil.cloneDelFirst("https://github.com/497446610/zk-config.git", "d:\\test", "master");
-		GitUtil.pull("d:\\test");
+		// GitUtil.cloneDelFirst("https://github.com/497446610/zk-config.git",
+		// "d:\\test", "master");
+
+		GitUtil.branchList("https://github.com/497446610/java-example-group.git");
 
 	}
 
